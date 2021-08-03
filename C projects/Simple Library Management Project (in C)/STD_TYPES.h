f141822acb91481ec640c8f21ebd014a0541bcb1
Mohamed Serag <mohamedserag.mes@gmail.com>
2021-08-03 02:49:56 +0200

    Adding C Practicing Projects

---
+++ C projects/Simple Library Management Project (in C)/STD_TYPES.h
@@ -0,0 +1,33 @@
+/********************************************************************************/
+/*  File Name: Std_Types.h                                                      */
+/*  File Info: This File Contains the standard data types new aliases for more  */
+/*             coding simplification and readability.                           */
+/*  Version  : V1.0                                                             */
+/*  Author   : Mohamed Serag                                                    */
+/*  Date     : 7 Oct 2019                                                       */
+/********************************************************************************/
+/******************************************************************/
+/* File Guard to prevent this header file from multiple inclusion */
+/******************************************************************/
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+
+/*    Definitions of the new unsigned interger types aliases    */
+typedef unsigned char        u8; 
+typedef unsigned short int  u16;
+typedef unsigned long int   u32;
+typedef unsigned long long  u64;
+
+/*    Definitions of the new signed interger types aliases      */
+typedef signed char          s8;
+typedef signed short int    s16;
+typedef signed long int     s32;
+
+/*    Definitions of the new Floating point types aliases    */
+typedef float               f32;
+typedef double              f64;
+typedef long double         f96;
+
+
+#endif /** the end of STD_TYPES_H */
